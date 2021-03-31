d663ea10879542e9a27499ad0bf595cd95b8f5ad
Faishal Eka (XI RPL 3) <faishaleka9@gmail.com>
2021-03-31 13:59:27 +0800

    Modul D: Mencetak Struk

--- AppResto/src/classes/Pesanan.java
+++ AppResto/src/classes/Pesanan.java
@@ -5,17 +5,23 @@
  */
 package classes;
 
-/**
- *
- * @author FaishalEka
- */
 public class Pesanan {
-    
     private Menu menu;
     private int jumlah;
     private String keterangan;
-    
-    public Pesanan (Menu menu, int jumlah) {}
-    public void setKeterangan(String keterangan) {}
-    public String getKeterangan() { return ""; }
+
+    public Pesanan(Menu menu, int jumlah){
+        this.menu = menu;
+        this.jumlah = jumlah;
+    }
+    public void setKeterangan(String attrib){
+        this.keterangan = attrib;
+    }
+    public String getKeterangan(){return "";}
+    public Menu getMenu(){
+        return menu;
+    }
+    public int getJumlah(){
+        return jumlah;
+    }
 }
